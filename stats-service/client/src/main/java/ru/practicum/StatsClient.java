27c1d854fecf51c40198a7e7361a3fb928bb7a10
Dima Bakinec <exwanted@gmail.com>
2023-07-19 12:42:17 +0300

    test 1

--- stats-service/client/src/main/java/ru/practicum/StatsClient.java
+++ stats-service/client/src/main/java/ru/practicum/StatsClient.java
@@ -1,6 +1,5 @@
 package ru.practicum;
 
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
@@ -9,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import ru.practicum.dto.dto.EndpointHit;
-import ru.practicum.dto.dto.ViewStats;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -32,22 +30,6 @@ public class StatsClient {
         this.client = WebClient.create(baseUrl);
     }
 
-//    public ResponseEntity<List<ViewStats>> getStats(String start, String end, List<String> uris, Boolean unique) {
-//        return this.client.get()
-//                .uri(uriBuilder -> uriBuilder
-//                        .path("/stats")
-//                        .queryParam("start", start)
-//                        .queryParam("end", end)
-//                        .queryParam("uris", uris)
-//                        .queryParam("unique", unique)
-//                        .build())
-//                .accept(MediaType.APPLICATION_JSON)
-//                .retrieve()
-//                .toEntityList(ViewStats.class)
-//                .doOnNext(c -> log.info("Get stats with param: start date {}, end date {}, uris {}, unique {}",
-//                        start, end, uris, unique))
-//                .block();
-//    }
 public ResponseEntity<Object> getStats(LocalDateTime start, LocalDateTime end, boolean unique, List<String> urls) {
     String paramsUri = String.join(",", urls);
     Map<String, Object> parameters = Map.of(
@@ -55,12 +37,11 @@ public ResponseEntity<Object> getStats(LocalDateTime start, LocalDateTime end, b
             "end", end.format(DATE_TIME_FORMATTER),
             "uris", paramsUri,
             "unique", unique);
-
     return (ResponseEntity<Object>) get(STATS_PATH + "?start={start}&end={end}&uris={uris}&unique={unique}", parameters.size());
 }
+
     public void saveStats(String app, String uri, String ip, LocalDateTime timestamp) {
         final EndpointHit endpointHit = new EndpointHit(app, uri, ip, timestamp);
-
         this.client.post()
                 .uri("/hit")
                 .contentType(MediaType.APPLICATION_JSON)
